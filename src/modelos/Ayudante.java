7e694ab59c88a6f0d557afa83764d5cf8238380c
gaveinti09 <gaveinti@espol.edu.ec>
2021-01-07 18:20:47 -0500

    Tecnica de Refactorizacion: Replace delegation with inheritancepara la clase Ayudante

--- src/modelos/Ayudante.java
+++ src/modelos/Ayudante.java
@@ -1,31 +1,7 @@
 package modelos;
 
-import java.util.ArrayList;
-
-public class Ayudante {
-    protected Estudiante est;
-    public ArrayList<Paralelo> paralelos;
-
-    Ayudante(Estudiante e){
-    	est = e;
-    }
-    public String getMatricula() {
-        return est.getMatricula();
-    }
-
-    public void setMatricula(String matricula) {
-        est.setMatricula(matricula);
-    }
-
-    //Getters y setters se delegan en objeto estudiante para no duplicar código
-    public String getNombre() {
-        return est.getNombre();
-    }
-
-    public String getApellido() {
-        return est.getApellido();
-    }
-
+public class Ayudante extends Estudiante{
+    
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
 
 
