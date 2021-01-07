c3e4e2b992fe1d510d6237e446dfc0063172dd8e
gaveinti09 <gaveinti@espol.edu.ec>
2021-01-07 18:17:03 -0500

    Tecnica: Encapsulate Field para clase Materia

--- src/modelos/Materia.java
+++ src/modelos/Materia.java
@@ -7,5 +7,54 @@ public class Materia {
     public double notaInicial;
     public double notaFinal;
     public double notaTotal;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public double getNotaInicial() {
+        return notaInicial;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public double getNotaTotal() {
+        return notaTotal;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public void setNotaInicial(double notaInicial) {
+        this.notaInicial = notaInicial;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
+    public void setNotaTotal(double notaTotal) {
+        this.notaTotal = notaTotal;
+    }
+    
     
 }
